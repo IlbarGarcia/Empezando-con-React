e73ab149db5b38684dbbec38338d792c7f351cbd
Ilbar Garcia <ilbarmolano@gmail.com>
2020-12-20 17:40:36 -0500

    recorrer array

--- src/Tarjeta.js
+++ src/Tarjeta.js
@@ -10,7 +10,7 @@ const Curso =({apellido,nombre})=> /*recibimos las propiedades y las desestructu
     <div className="card-container">
       <div className="header">
         <a href="">
-          <img src='https://lh3.googleusercontent.com/proxy/5OGshVCrnpBE37pP08458Tdjynee-qwT1CZXQzw9AaWCpTZzsLa55UBfXGri8nxbMKRn2K7r6zMvlxCVbomUqqR_WSGU6L0hbUf4Qk7oj9OUB7a_H-1VjSxaFd0ogdRqLTQC-fAK' alt=""/>
+          <img src='https://lh3.googleusercontent.com/proxy/i5OGshVCrnpBE37pP08458Tdjynee-qwT1CZXQzw9AaWCpTZzsLa55UBfXGri8nxbMKRn2K7r6zMvlxCVbomUqqR_WSGU6L0hbUf4Qk7oj9OUB7a_H-1VjSxaFd0ogdRqLTQC-fAK' alt=""/>
         </a>
         <h2>{`${nombre} ${apellido}`}</h2> {/*dentro de las llaves podemos escribir javascript puro, es decir podemos hacer referncia a una variable creada o a un obejto o a una funcion , cualquier cosa que sea javacript puro, dentro de las llaves recibimos el valor de nombre que recibimos como props  */}
         <h3>{edadUsuario(usuario.edad)?<h1>es mayor</h1>:<h3>es menor</h3>}</h3> {/*aqui dentro de las llaves llamamos la funcion, y utilizamos operadores ternarios para validar, ya que no podemos usar if observemos que en el opreador ternario podemos devolver un texto un elemento o un componente  */}
